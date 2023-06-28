82388a35958a21c13bc7a9065cd9a8ad94dedf4d
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-06-28 23:31:50 +0200

    new project FoodRide a web app to order food

---
+++ src/components/UI/ButtonPrimary.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./ButtonPrimary.css";
+function ButtonPrimary(props) {
+  return <button className="btn-primary">Proceed to Checkout</button>;
+}
+
+export default ButtonPrimary;
